c5bb672a56d2aa31a37bf476c016b5390796acb4
Andrew-Veteto <78765133+Andrew-Veteto@users.noreply.github.com>
2022-03-21 17:11:23 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+new Vue({
+    el: "#flashcards-app",
+    data: {
+      cards:[
+        {
+          question:"What year was IUPUI founded?",
+          answer:"1969",
+          category:"history",
+          flipped:false
+        },
+        {
+          question:"What is 2+2?",
+          answer:"Fish",
+          category:"math",
+          flipped:false
+        },{
+          question:"Is mayonnaise an instrument?",
+          answer:"No",
+          category:"science",
+          flipped:false
+        }
+      ],
+      form:{
+        question:"",
+        answer:"",
+        category:"",
+        flipped:false
+      },
+    },
+
+    methods:{
+        changeSide: function(card){
+          card.flipped = !card.flipped;
+        },
+
+        submit: function() {
+          this.cards.push(this.form);
+          this.form={
+            question:"",
+            answer:"",
+            category:"",
+            flipped:false
+
+          }
+        },
+
+    }
+})
